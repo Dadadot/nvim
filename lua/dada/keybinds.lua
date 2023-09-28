c1731e9811eca96f8f5554a1d0de51cb3cee7fd3
Peter Meimer <reformedagnostic@gmail.com>
2023-09-28 20:42:17 +0200

    add ctrl + direction resize mappings to keymap.lua (or sth like that)

--- lua/dada/keybinds.lua
+++ lua/dada/keybinds.lua
@@ -8,6 +8,11 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- Ctrl + direciton to decrease/increase the width of the current window
+vim.api.nvim_set_keymap('n', '<C-h>', ':vertical resize -2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', ':vertical resize +2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', ':resize -2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', ':resize +2<CR>', { noremap = true, silent = true })
 
 -- costum keymaps
 
